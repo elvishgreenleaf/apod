27658afaafe10b3174a8073583fc0ed2c2898ce9
elvishgreenleaf <66024433+elvishgreenleaf@users.noreply.github.com>
2020-05-28 18:54:07 +0530

    Update something.py

--- something.py
+++ something.py
@@ -11,18 +11,18 @@ else:
     imglink = 'https://apod.nasa.gov/apod/' + response['src']
 def main():
 	while True:
-	    sess = rq.Session()
-	    cid='@testchaneling'
-	    turl = 'https://api.telegram.org/bot1157710197:AAGzLcuOqieGigo_Bo3UafXL3LKjLsrfL5Q/'
-	    if response == None:
-		imglink = soup.find('iframe')['src']
-		params = {'chat_id':cid,'text':imglink}
-		sess.post(turl + 'sendMessage', data=params)
-	    else:
-		imglink = 'https://apod.nasa.gov/apod/' + response['src']
-		title = soup.find('b').get_text()
-		params = {'chat_id':cid,'photo':imglink,'caption':title}
-		sess.post(turl + 'sendPhoto', data=params)
-	    time.sleep(30)
+	    	sess = rq.Session()
+	    	cid='@testchaneling'
+	    	turl = 'https://api.telegram.org/bot1157710197:AAGzLcuOqieGigo_Bo3UafXL3LKjLsrfL5Q/'
+	    	if response == None:
+			imglink = soup.find('iframe')['src']
+			params = {'chat_id':cid,'text':imglink}
+			sess.post(turl + 'sendMessage', data=params)
+	    	else:
+			imglink = 'https://apod.nasa.gov/apod/' + response['src']
+			title = soup.find('b').get_text()
+			params = {'chat_id':cid,'photo':imglink,'caption':title}
+			sess.post(turl + 'sendPhoto', data=params)
+	    	time.sleep(30)
 if __name__ == '__main__':
     main()
