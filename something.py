cef7ae3b6556c638f0e2d6a61a4814639ca71218
elvishgreenleaf <66024433+elvishgreenleaf@users.noreply.github.com>
2020-05-28 20:01:45 +0530

    Update something.py

--- something.py
+++ something.py
@@ -6,23 +6,22 @@ page = rq.get(url).content
 soup = bs(page, 'html.parser')
 response = soup.find('img')
 if response == None:
-    imglink = soup.find('iframe')['src']
+	imglink = soup.find('iframe')['src']
 else:
-    imglink = 'https://apod.nasa.gov/apod/' + response['src']
-def main():
-	while True:
-	    sess = rq.Session()
-	    cid='@testchaneling'
-	    turl = 'https://api.telegram.org/bot1157710197:AAGzLcuOqieGigo_Bo3UafXL3LKjLsrfL5Q/'
-	    if response == None:
-		    imglink = soup.find('iframe')['src']
-		    params = {'chat_id':cid,'text':imglink}
-		    sess.post(turl + 'sendMessage', data=params)
-	    else:
-		    imglink = 'https://apod.nasa.gov/apod/' + response['src']
-		    title = soup.find('b').get_text()
-		    params = {'chat_id':cid,'photo':imglink,'caption':title}
-		    sess.post(turl + 'sendPhoto', data=params)
-	    time.sleep(30)
-if __name__ == '__main__':
-    main()
+	imglink = 'https://apod.nasa.gov/apod/' + response['src']
+def work():
+	sess = rq.Session()
+	cid='@testchaneling'
+	turl = 'https://api.telegram.org/bot1157710197:AAGzLcuOqieGigo_Bo3UafXL3LKjLsrfL5Q/'
+	if response == None:
+		imglink = soup.find('iframe')['src']
+		params = {'chat_id':cid,'text':imglink}
+		sess.post(turl + 'sendMessage', data=params)
+	else:
+		imglink = 'https://apod.nasa.gov/apod/' + response['src']
+		title = soup.find('b').get_text()
+		params = {'chat_id':cid,'photo':imglink,'caption':title}
+		sess.post(turl + 'sendPhoto', data=params)
+	time.sleep(30)
+while True :
+	work()
